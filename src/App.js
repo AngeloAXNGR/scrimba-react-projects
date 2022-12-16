eef9bc8d701bcf57283321eddb43767179a1e482
LuhxEn <axngrsantos@gmail.com>
2022-12-16 10:29:24 +0800

    Add starter files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {Component, useState, useEffect} from 'react';
-import Header from './components/Header';
-import Journal from './components/Journal';
-import data from './data/data';
 
-const travelCards = data.map(item =>{
-  return(
-    <Journal
-      {...item}
-    />
-  )
-})
+
 
 const App = () => {
 
   return(
     <div className="container">
-      <Header
-        icon="globe-icon.png"
-      />
-
-      <div className="journals">
-        {travelCards}
-      </div>
+      Hello World
     </div>
 
   );
