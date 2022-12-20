69891d9f9969a4e8eb4a334a6f898eceab904f15
LuhxEn <axngrsantos@gmail.com>
2022-12-20 12:07:25 +0800

    Finish Meme Generator app

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,17 +1,61 @@
-import memesData from '../data/memesData';
 import React from 'react';
 
 
 
 const Meme = () =>{
-
   const [meme, setMeme] = React.useState({
-    topText: "Shut Up",
-    bottomText: "And Take My Money!",
+    topText: "",
+    bottomText: "",
     randomImage: "https://i.imgflip.com/3si4.jpg"
   });
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemeImages, setAllMemeImages] = React.useState([])
+
+
+
+    /**
+   * Challenge: 
+   * As soon as the Meme component loads the first time,
+   * make an API call to "https://api.imgflip.com/get_memes".
+   * 
+   * When the data comes in, save just the memes array part
+   * of that data to the `allMemes` state
+   * 
+   * Think about if there are any dependencies that, if they
+   * changed, you'd want to cause to re-run this function.
+   * 
+   * Hint: for now, don't try to use an async/await function.
+   * Instead, use `.then()` blocks to resolve the promises
+   * from using `fetch`. We'll learn why after this challenge.
+   */
+
+  /**
+  useEffect takes a function as its parameter. If that function
+  returns something, it needs to be a cleanup function. Otherwise,
+  it should return nothing. If we make it an async function, it
+  automatically retuns a promise instead of a function or nothing.
+  Therefore, if you want to use async operations inside of useEffect,
+  you need to define the function separately inside of the callback
+  function, as seen below:
+  */
+  React.useEffect(()=>{
+    async function getMemes(){
+      const res = await fetch("https://api.imgflip.com/get_memes")
+      const data = await res.json();
+      // Saving fetched data into a state
+      setAllMemeImages(data.data.memes);
+    }
+
+    getMemes();
+    console.log('test');
+
+    // Clean up function (optional function, can be removed)
+    return () =>{
+
+    }
+  }, []) // Don't need a specific dependency since I only need to fetch once
+
+  console.log(allMemeImages)
 
   function handleInput(event){
     console.log(meme);
@@ -26,7 +70,7 @@ const Meme = () =>{
 
   function getMemeImage(event){
     event.preventDefault();
-    const memesArray = allMemeImages.data.memes
+    const memesArray = allMemeImages
     const randomNumber = Math.floor((Math.random() * memesArray.length));
     const url = memesArray[randomNumber].url
 
