1d6ca928feeb35099785d90d168f873abd3514d2
LuhxEn <axngrsantos@gmail.com>
2022-12-23 17:49:42 +0800

    Create Start Menu UI

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import QuizForm from "./components/QuizForm";
 
 export default function App() {
 
-	const [hasStarted, setHasStarted] = React.useState(false);
+	const [hasStarted, setHasStarted] = React.useState(true);
 
 
 	function toggleScreen(){
 		setHasStarted(prevStarted => {return !prevStarted});
 	}
+
+
+	const StartScreen = (props) =>{
+		return(
+			<div className="start-screen">
+				<h1>Quiz App</h1>
+				<button className="start-button" onClick={props.handleClick}>Start</button>
+			</div>
+		);
+	}
 	
 	return (
 		<main>
-			<QuizForm/>
+			{hasStarted ? <StartScreen handleClick={toggleScreen}/> : <QuizForm/>}
 		</main>
 	)
 }
