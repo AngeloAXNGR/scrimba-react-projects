c8cf755b8a61452d834f0ea62dc7b83183768804
LuhxEn <axngrsantos@gmail.com>
2022-12-23 09:24:24 +0800

    Add function to retrieve and display trivia data

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -1,8 +1,9 @@
 import { useState , useEffect} from "react";
-const Quiz = (props) => {
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import { nanoid } from "nanoid";
+
+const QuizForm = () => {
   const [questions, setQuestions] = useState([]);
-  // https://opentdb.com/api.php?amount=5&type=multiple&encode=url3986
-  // https://opentdb.com/api.php?amount=5&type=multiple
 
   useEffect(()=>{
     async function getQuestions(){
@@ -13,13 +14,21 @@ const Quiz = (props) => {
         const randomizedIndex = Math.floor(Math.random() * result.incorrect_answers.length)
         let choicesArray = result.incorrect_answers
         choicesArray.splice(randomizedIndex, 0, result.correct_answer);
+
+        let selection = []
+        for (let i = 0; i < choicesArray.length; i++){
+          selection.push({id:nanoid(), value:choicesArray[i],selected:false})
+        }
+
         return {
+          id: nanoid(),
           question: result.question,
           correctAnswer: result.correct_answer,
-          choices: choicesArray,
+          choices: selection,
         }
       })
 
+      // console.log(test);
       setQuestions(revisedData)
     }
 
@@ -27,24 +36,30 @@ const Quiz = (props) => {
   },[])
 
   const Question = (props) =>{
+    const choices = props.choices.map(choice => <div className="choice">{choice.value}</div>)
     return(
       <div className="question">
         <h1>{props.question}</h1>
+        <div className="choices">
+          {choices}
+        </div>
       </div>
     )
   }
 
   const questionElements = questions.map(question => {
-    return <Question question={question.question}/>
+    const parsedQuestion = ReactHtmlParser(question.question)
+    return <Question question={parsedQuestion} choices={question.choices}/>
   })
   
   console.log(questions)
   return(
     <div className="quiz-form">
-      {questionElements}  
+      <div className="questions">{questionElements} </div>
+      <button className="check-answers">Check Answers</button>
     </div>
   );
 }
 
 
-export default Quiz
+export default QuizForm
